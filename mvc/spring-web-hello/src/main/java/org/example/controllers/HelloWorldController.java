efe85652cf3015efcabbc29e85178ee923e81a9f
rabindrapatra <rabindrapatra@corp.com>
2024-07-17 18:13:14 +0530

    hello world spring mvc and jsp

---
+++ mvc/spring-web-hello/src/main/java/org/example/controllers/HelloWorldController.java
@@ -0,0 +1,18 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView helloWorld(@RequestParam("city") String city) {
+        String message = "Spring, Hello, World! "+city;
+        return new ModelAndView("hello", "message", message);
+    }
+}
