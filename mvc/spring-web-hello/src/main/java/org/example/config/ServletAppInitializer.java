02e859ca59973640c031a99d9f2d6e6cee2530c3
rabindrapatra <rabindrapatra@corp.com>
2024-07-18 20:56:47 +0530

    rename application

--- mvc/spring-web-hello/src/main/java/org/example/config/ServletAppInitializer.java
+++ mvc/spring-web-hello/src/main/java/org/example/config/ServletAppInitializer.java
@@ -8,7 +8,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class AppInitializer implements WebApplicationInitializer {
+public class ServletAppInitializer implements WebApplicationInitializer {
 
   @Override
   public void onStartup(ServletContext servletContext) throws ServletException {
